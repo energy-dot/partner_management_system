4806d63d462582f856f591c4958d19336d80e126
energy-dot <142364162+energy-dot@users.noreply.github.com>
2025-04-03 14:27:37 +0900

    rev1

---
+++ frontend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+  testMatch: ['<rootDir>/src/tests/**/*.test.(ts|tsx)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
